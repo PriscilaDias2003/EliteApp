2ec8a309822a913c9d50f4d83055fae0be1e69de
Priscila Dias <62040835+PriscilaDias2003@users.noreply.github.com>
2021-06-23 20:49:36 -0300

    Rota InfoClient adicionada

--- FrontEnd/src/Routes.js
+++ FrontEnd/src/Routes.js
@@ -5,14 +5,16 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/HomePage';
 import SignUp from './pages/SignUp/index';
+import InfoClient from './pages/InfoClient';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component= {Home} />
-                <Route path="/login" component= {Login} />
-                <Route path="/signup" component= {SignUp} />
+                <Route path="/" exact component = {Home} />
+                <Route path="/login" component = {Login} />
+                <Route path="/signup" component = {SignUp} />
+                <Route path="/info" component = {InfoClient} />
             </Switch>
         </BrowserRouter>
     );
